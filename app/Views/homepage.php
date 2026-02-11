f5b55fd79554742241952cc5f2872c522e12a5b1
Sunny420x <costerraid@gmail.c.com>
2026-02-11 15:09:50 +0700

    feat: first commit

---
+++ app/Views/homepage.php
@@ -0,0 +1,155 @@
+<html>
+
+<head>
+    <title>IronPDF | Building on the success of IronPDF for .NET<</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/style.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="og:title" content="IronPDF | Building on the success of IronPDF for .NET">
+    <meta name="og:description" content="IronPDF Beta Program also coming soon for Python | Node.JS | Java">
+</head>
+
+<body>
+    <div class="">
+        <div class="header">
+            <div class="navigator p-3">
+                <img src="image/LOGO WHITE.svg" alt="">
+                <ul>
+                    <li>Products <img src="image/arrow.svg" class="float-end p-1 ps-2"></li>
+                    <li>About Us</li>
+                    <li>Career</li>
+                </ul>
+            </div>
+            <div class="container">
+                <div class="content">
+                    <img src="image/logo.svg" alt="" class="mb-4">
+                    <p class="mt-4">Building on the success of IronPDF for .NET</p>
+                    <span class="header-text">Beta Software Program</span><br>
+                    <span class="header-text with-color">IronPDF for C++</span>
+                </div>
+            </div>
+            <img src="image/ribbon.png" class="ribbon" alt="">
+            <div class="section">
+                <div class="sign-up-now">
+                    <div class="container">
+                        <h1>Be one of the first</h1>
+                        <h2>Sign up NOW to get early access!</h2>
+                        <div class="sign-up-now-input">
+                            <input type="email" placeholder="Enter email address">
+                            <button>Sign up now</button>
+                        </div>
+                        <div class="coming-soon d-flex">
+                            <div class="coming-soon-badge mt-4"># Coming Soon</div>
+                            <span>IronPDF Beta Program also coming soon for Python | Node.JS | Java</span>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="about">
+                    <div class="header">
+                        <div class="container">
+                            <h1 class="mb-5">IronPDF for C++
+                                <span class="intersect">
+                                    <img src="image/coming-soon-badge.svg" alt="">
+                                </span>
+                            </h1>
+                            <div class="row mt-4">
+                                <div class="col-lg"># Generate PDFs from HTML in C++ <div class="divider-3 float-end">
+                                    </div>
+                                </div>
+                                <div class="col-lg"># Combine, split, and modify PDFs quickly in C++ <div
+                                        class="divider-3 float-end"></div>
+                                </div>
+                                <div class="col-lg"># Extract text and images from PDFs using C++</div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="content">
+                        <div class="container">
+                            <p>
+                                The new <b>IronPDF library for C++</b> will enhance the C++ developer’s tool set with
+                                robust PDF generation and conversion capabilities. Developers will be able to generate
+                                new PDFs from HTML content (from text or from URL), as well as to combine, split,
+                                extract, and modify content from existing PDFs.
+                            </p>
+                            <p>
+                                <b>IronPDF for C++</b> will help developers create C++ applications that can do all of
+                                these PDF processing tasks and more, with speed, precision, control, and excellence.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="why-make-csharp">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-lg-auto">
+                                <img src="image/HTML to PDF icon.svg" alt="">
+                            </div>
+                            <div class="col-lg ps-5">
+                                <h2>Why make a C++ PDF Library</h2>
+                                <p>C++ is one of the most popular, oldest, and important programming languages in use, being
+                                    the language of choice in low-level systems and network programming and other domains
+                                    where performance is critical.</p>
+                                <p>The release of IronPDF for C++ will aid developers in building performant applications
+                                    that can carry out PDF-related processing tasks</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="early-access">
+                    <div class="container">
+                        <span>Early Access to <span class="with-color">C++ PDF Library</span></span>
+                        <p>Joining the early access program will allow you to collaborate closely with our
+                            engineering team. You will be playing a key role in the development process as you share
+                            your early experiences using the C++ PDF library before its official launch. Your
+                            continual feedback after we release the library will be immensely helpful as we release
+                            new features and improve on existing features.</p>
+                        <div class="row row-cols-3 gap-3 g-0">
+                            <div class="item col">
+                                <div class="row">
+                                    <div class="release-badge col-auto"># Released</div>
+                                    <div class="name col">
+                                        <h3>IRONPDF</h3>
+                                        <h4>for Java</h4>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="item col">
+                                <div class="row">
+                                    <div class="coming-soon-badge col-auto"># Coming Soon</div>
+                                    <div class="name col">
+                                        <h3>IRONPDF</h3>
+                                        <h4>for Java</h4>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="item col">
+                                <div class="row">
+                                    <div class="coming-soon-badge col-auto"># Coming Soon</div>
+                                    <div class="name col">
+                                        <h3>IRONPDF</h3>
+                                        <h4>for Java</h4>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="footer-sign-up">
+                        <span>Sign up to our <span class="with-color">Beta Program</span></span>
+                        <div class="sign-up-now-input center mt-4">
+                            <input type="email" placeholder="Enter email address">
+                            <button>Sign up now</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI"
+        crossorigin="anonymous"></script>
+</body>
+
+</html>
