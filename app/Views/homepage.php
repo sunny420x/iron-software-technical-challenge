477da2bb00f9c69f9234b47755f0dea70a3db92a
Sunny420x <costerraid@gmail.c.com>
2026-02-16 11:12:04 +0700

    homepage update: self close tag

--- app/Views/homepage.php
+++ app/Views/homepage.php
@@ -1,10 +1,10 @@
 <html>
 
 <head>
-    <title>IronPDF | Building on the success of IronPDF for .NET<</title>
+    <title>IronPDF | Building on the success of IronPDF for .NET</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
-    <link rel="stylesheet" href="css/style.css">
+        integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous" />
+    <link rel="stylesheet" href="css/style.css" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <meta name="og:title" content="IronPDF | Building on the success of IronPDF for .NET" />
     <meta name="og:description" content="IronPDF Beta Program also coming soon for Python | Node.JS | Java" />
